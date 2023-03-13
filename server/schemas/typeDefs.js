ccc60eee199a97630711702f17daccca4003684f
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-03-12 21:07:55 -0400

    add typeDefs based on models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,22 +1,56 @@
-const router = require('express').Router();
-const {
-  createUser,
-  getSingleUser,
-  saveBook,
-  deleteBook,
-  login,
-} = require('../../controllers/user-controller');
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+
+  }
+`;
+  //createUser,
+ // getSingleUser,
+  //saveBook,
+  //deleteBook,
+  //login,
 
 // import middleware
-const { authMiddleware } = require('../../utils/auth');
+// const { authMiddleware } = require('../../utils/auth');
 
-// put authMiddleware anywhere we need to send a token for verification of user
-router.route('/').post(createUser).put(authMiddleware, saveBook);
+// // put authMiddleware anywhere we need to send a token for verification of user
+// router.route('/').post(createUser).put(authMiddleware, saveBook);
 
-router.route('/login').post(login);
+// router.route('/login').post(login);
 
-router.route('/me').get(authMiddleware, getSingleUser);
+// router.route('/me').get(authMiddleware, getSingleUser);
 
-router.route('/books/:bookId').delete(authMiddleware, deleteBook);
+// router.route('/books/:bookId').delete(authMiddleware, deleteBook);
 
-module.exports = router;
+module.exports = typeDefs;
