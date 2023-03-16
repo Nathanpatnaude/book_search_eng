2356420f201d765693b4f6c1bc28dc36a480928a
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-03-16 03:43:18 -0400

    removed get

--- server/server.js
+++ server/server.js
@@ -25,9 +25,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("/", (req, res) => {
-  res.sendFile([path.join(__dirname, "../client/")]);
-})
+// app.get("/", (req, res) => {
+//   res.sendFile([path.join(__dirname, "../client/")]);
+// })
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
